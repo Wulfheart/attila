f583c3c3efbab6048728484b347985cd3084d5b9
wulfheart <dev@alexfwulf.de>
2020-11-07 14:52:49 +0000

    Test

--- public/js/app.js
+++ public/js/app.js
@@ -19384,8 +19384,8 @@ window.axios.defaults.headers.common['X-Requested-With'] = 'XMLHttpRequest';
 /*! no static exports found */
 /***/ (function(module, exports, __webpack_require__) {
 
-__webpack_require__(/*! /home/alex/www/attila/resources/js/app.js */"./resources/js/app.js");
-module.exports = __webpack_require__(/*! /home/alex/www/attila/resources/css/app.css */"./resources/css/app.css");
+__webpack_require__(/*! /home/pi/www/attila/resources/js/app.js */"./resources/js/app.js");
+module.exports = __webpack_require__(/*! /home/pi/www/attila/resources/css/app.css */"./resources/css/app.css");
 
 
 /***/ })
