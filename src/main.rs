a21ad75c016b6e4f2ae7101ab31c845501f4903a
Alex Wulf <alex.f.wulf@gmail.com>
2022-03-26 17:07:41 +0100

    Displaying Readme

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,19 @@
-fn main() {
-    println!("Hello, world!");
+#[macro_use] extern crate rocket;
+use multi_skill::systems::SimpleEloMMR;
+use rocket::{serde, serde::json};
+use rocket::response::content;
+
+static README: &'static str = include_str!("../README.md");
+
+#[get("/")]
+fn index() -> content::Html<String> {
+    let html = comrak::markdown_to_html(README, &comrak::ComrakOptions::default());
+    content::Html(html)
+}
+
+
+
+#[launch]
+fn rocket() -> _ {
+    rocket::build().mount("/", routes![index])
 }
