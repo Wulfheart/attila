602889fa5efadd46f03c9837a254013793edddd0
Alex Wulf <alex.f.wulf@gmail.com>
2022-03-26 22:32:54 +0100

    WIP

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,10 @@
 #[macro_use] extern crate rocket;
-use multi_skill::systems::SimpleEloMMR;
-use rocket::{serde, serde::json};
+use multi_skill::data_processing::{Wrap, Dataset};
+use multi_skill::experiment_config::Experiment;
+use multi_skill::systems::{SimpleEloMMR, get_rating_system_by_name};
+use rocket::serde::json::serde_json::Result;
+use rocket::serde::{Deserialize, json::Json};
+use rocket::serde;
 use rocket::response::content;
 
 static README: &'static str = include_str!("../README.md");
@@ -11,9 +15,67 @@ fn index() -> content::Html<String> {
     content::Html(html)
 }
 
+#[derive(Deserialize, Clone)]
+#[serde(crate = "rocket::serde")]
+struct Standing {
+    player: String,
+    low_rank: usize,
+    high_rank: usize
+}
+
+#[derive(Deserialize, Clone)]
+#[serde(crate = "rocket::serde")]
+struct Contest {
+    name: String,
+    unix_time: u64,
+    weight: f64,
+    standings: Vec<Standing>
+}
+
+#[derive(Deserialize)]
+#[serde(crate = "rocket::serde")]
+struct Request {
+    contests: Vec<Contest>
+}
+
+#[post("/", data="<req>")]
+fn post(req: Json<Request>) -> Json<usize> {
+    let length = req.contests.iter().count();
+    let mut res: Vec<multi_skill::data_processing::Contest> = req.contests.iter().map(|c|multi_skill::data_processing::Contest {
+        name: c.name,
+        standings: c.standings.iter().map(|s| (s.player.clone(), s.low_rank, s.high_rank)).collect(),
+        url: None,
+        time_seconds: c.unix_time,
+        weight: c.weight
+    }).collect();
+    res.sort_by_key(|c| c.time_seconds);
+
+
+    let dataset = Wrap::from_closure(length, move |i| {
+        let c = req.contests[i].clone();
+        multi_skill::data_processing::Contest {
+                    name: c.name,
+                    standings: c.standings.iter().map(|s| (s.player.clone(), s.low_rank, s.high_rank)).collect(),
+                    url: None,
+                    time_seconds: c.unix_time,
+                    weight: c.weight
+        }
+    }).boxed();
+    let experiment = Experiment {
+        mu_noob: 1500.,
+        sig_noob: 350.,
+        system: Box::new(SimpleEloMMR::default()),
+        dataset: dataset,
+    };
+
+    experiment.
+ 
+    return Json()
+}
+
 
 
 #[launch]
 fn rocket() -> _ {
-    rocket::build().mount("/", routes![index])
+    rocket::build().mount("/", routes![index, post])
 }
